d0848c99968581d19e4350e0b8d2db01ee1defc4
musakhudiyev <musakhudiyev@gmail.com>
2022-12-18 20:18:02 +0400

    add functions delete

--- ui/components/garments/hooks.ts
+++ ui/components/garments/hooks.ts
@@ -6,17 +6,14 @@ import { wardrobeActions } from '@/stores/wardrobe'
 
 const variants = {
     init: { opacity: 0, scale: 0.5 },
-    animate: { opacity: 1, scale: 1 },
+    animate: {
+        opacity: 1,
+        scale: 1,
+    },
     transition: {
         default: {
             duration: 0.3,
             ease: [0, 0.71, 0.2, 1.01],
-        },
-        scale: {
-            type: 'spring',
-            damping: 5,
-            stiffness: 100,
-            restDelta: 0.001,
         },
     },
     exit: { opacity: 0, scale: 0.5 },
