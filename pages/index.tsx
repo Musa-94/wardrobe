b795ec16c45d52979f52faf72a9e041496fa5f85
musakhudiyev <musakhudiyev@gmail.com>
2022-12-28 12:16:28 +0400

    delete meta teg viewport

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,6 @@ export default function HomePage() {
         <>
             <Head>
                 <title>wardrobe</title>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1, maximum-scale=1"
-                />
             </Head>
             <Wardrobe />
         </>
